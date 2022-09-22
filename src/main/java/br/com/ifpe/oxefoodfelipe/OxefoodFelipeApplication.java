af00afbabb36f0bf9331f50b20bcb01f5e698231
Felipe Vieira da Rocha <felipevieiradarocha@hotmail.com>
2022-09-22 20:48:12 -0300

    arquivos do projeto

---
+++ src/main/java/br/com/ifpe/oxefoodfelipe/OxefoodFelipeApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefoodfelipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodFelipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodFelipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifpe/oxefoodfelipe/OxefoodFelipeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefoodfelipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxefoodFelipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
