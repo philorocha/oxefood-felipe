5f5478adcd7c3d1bff58c95c7e6fd9d1657fbfbd
aluno <aluno@.ifpe>
2022-10-18 21:37:59 -0300

    Implementando inclusao aula 181022

--- src/main/java/br/com/ifpe/oxefoodfelipe/modelo/cliente/Cliente.java
+++ src/main/java/br/com/ifpe/oxefoodfelipe/modelo/cliente/Cliente.java
@@ -1,5 +1,44 @@
 package br.com.ifpe.oxefoodfelipe.modelo.cliente;
 
-public class Cliente {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Where;
+
+import br.com.ifpe.oxefoodfelipe.util.entity.EntidadeAuditavel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Cliente")
+@Where(clause = "habilitado = true")
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cliente extends EntidadeAuditavel{/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1073125754768164853L;
+	
+	@Column
+	private String chaveEmpresa;
+	
+	@Column
+	private String nome;
+	
+	@Column
+	private String cpf;
+	
+	@Column
+	private String fone;
+	
+	@Column
+	private String foneAlternativo;
 
 }
--- src/main/java/br/com/ifpe/oxefoodfelipe/modelo/cliente/ClienteService.java
+++ src/main/java/br/com/ifpe/oxefoodfelipe/modelo/cliente/ClienteService.java
@@ -1,5 +1,22 @@
 package br.com.ifpe.oxefoodfelipe.modelo.cliente;
 
-public class ClienteService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.ifpe.oxefoodfelipe.modelo.produto.CategoriaProduto;
+import br.com.ifpe.oxefoodfelipe.util.entity.GenericService;
+
+@Service
+public class ClienteService extends GenericService{
+	
+	@Autowired
+    private ClienteRepository repository;
+	
+	@Transactional
+    public Cliente save(Cliente cliente) {
+		super.preencherCamposAuditoria(cliente);
+		return repository.save(cliente);
+    }
 
 }
